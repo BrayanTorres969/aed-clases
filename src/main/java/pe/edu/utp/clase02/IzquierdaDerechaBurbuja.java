29c740ea892fef19a965046defe3854a518cefe8
BrayanT <brayantorresacuna2023@gmail.com>
2023-08-22 20:56:31 -0500

    caso 1

---
+++ src/main/java/pe/edu/utp/clase02/IzquierdaDerechaBurbuja.java
@@ -0,0 +1,24 @@
+package pe.edu.utp.clase02;
+
+public class IzquierdaDerechaBurbuja {
+    public static void main(String[] args) {
+
+        //Bubuja de Izquierda a derecha
+        int[] datos = {3, 2, 4, 1, 2,0,7,1};
+        for (int i = datos.length - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                if (datos[j] > datos[j + 1]) {
+                    int aux = datos[j];
+                    datos[j] = datos[j + 1];
+                    datos[j + 1] = aux;
+
+                }
+            }
+        }
+
+        for (int i = 0; i < datos.length; i++) {
+            System.out.print(datos[i]+" ");
+        }
+
+    }
+}
--- src/main/java/pe/edu/utp/clase02/Main.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.utp.clase02;
-
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome!");
-
-    }
-}

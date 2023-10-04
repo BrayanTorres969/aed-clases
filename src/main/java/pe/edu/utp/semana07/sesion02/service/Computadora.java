126ee3788df6bbe0750f48d25cee9ac96c4aea66
BrayanTorres <brayantorresacuna2023@gmail.com>
2023-10-03 23:02:33 -0500

    :hammer: Operaciones con listas enlazadas

---
+++ src/main/java/pe/edu/utp/semana07/sesion02/service/Computadora.java
@@ -0,0 +1,43 @@
+package pe.edu.utp.semana07.sesion02.service;
+
+public class Computadora {
+    private String codigo;
+    private String marca;
+    private String modelo;
+    private double precio;
+    //Constructores
+    public Computadora() {
+
+    }
+    public Computadora(String codigo, String marca, String modelo, double precio) {
+        this.codigo = codigo;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+    }
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+}
